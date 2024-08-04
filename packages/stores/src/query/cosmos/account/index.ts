2b97c8f367a0fb7fd951232958aca3994ab12467
toan <toan.dq@orai.io>
2024-08-04 23:31:23 +0700

    update account cosmos

--- packages/stores/src/query/cosmos/account/index.ts
+++ packages/stores/src/query/cosmos/account/index.ts
@@ -2,21 +2,21 @@ import {
   ObservableChainQuery,
   ObservableChainQueryMap,
 } from "../../chain-query";
-import { KVStore } from "@owallet/common";
-import { ChainGetter } from "../../../common";
+import { ChainGetter } from "../../../chain";
 import { AuthAccount } from "./types";
 import { computed, makeObservable } from "mobx";
 import { BaseAccount } from "@owallet/cosmos";
+import { QuerySharedContext } from "../../../common";
 
 export class ObservableQueryAccountInner extends ObservableChainQuery<AuthAccount> {
   constructor(
-    kvStore: KVStore,
+    sharedContext: QuerySharedContext,
     chainId: string,
     chainGetter: ChainGetter,
     protected readonly bech32Address: string
   ) {
     super(
-      kvStore,
+      sharedContext,
       chainId,
       chainGetter,
       `/cosmos/auth/v1beta1/accounts/${bech32Address}`
@@ -25,11 +25,21 @@ export class ObservableQueryAccountInner extends ObservableChainQuery<AuthAccoun
     makeObservable(this);
   }
 
+  protected override canFetch(): boolean {
+    // If bech32 address is empty, it will always fail, so don't need to fetch it.
+    return this.bech32Address.length > 0;
+  }
+
   @computed
   get sequence(): string {
     if (!this.response) {
       return "0";
     }
+
+    // XXX: In launchpad, the status was 200 even if the account not exist.
+    //      However, from stargate, the status becomes 404 if the account not exist.
+    //      This case has not been dealt with yet.
+    //      However, in the case of 404, it will be treated as an error, and in this case the sequence should be 0.
 
     try {
       const account = BaseAccount.fromProtoJSON(
@@ -41,17 +51,26 @@ export class ObservableQueryAccountInner extends ObservableChainQuery<AuthAccoun
       return "0";
     }
   }
+
+  @computed
+  get isVestingAccount(): boolean {
+    if (!this.response) {
+      return false;
+    }
+
+    return !!this.response.data?.account.base_vesting_account;
+  }
 }
 
 export class ObservableQueryAccount extends ObservableChainQueryMap<AuthAccount> {
   constructor(
-    protected readonly kvStore: KVStore,
-    protected readonly chainId: string,
-    protected readonly chainGetter: ChainGetter
+    sharedContext: QuerySharedContext,
+    chainId: string,
+    chainGetter: ChainGetter
   ) {
-    super(kvStore, chainId, chainGetter, (bech32Address) => {
+    super(sharedContext, chainId, chainGetter, (bech32Address) => {
       return new ObservableQueryAccountInner(
-        this.kvStore,
+        this.sharedContext,
         this.chainId,
         this.chainGetter,
         bech32Address
