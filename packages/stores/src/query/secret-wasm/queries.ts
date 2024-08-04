4fd6d6cfdfebde4215474e59658bb4ba6bebd4e7
toan <toan.dq@orai.io>
2024-08-04 23:51:32 +0700

    done for secret-swasm new base

--- packages/stores/src/query/secret-wasm/queries.ts
+++ packages/stores/src/query/secret-wasm/queries.ts
@@ -1,65 +1,72 @@
 import { QueriesSetBase } from "../queries";
-import { ChainGetter } from "../../common";
-import { KVStore } from "@owallet/common";
+import { ChainGetter } from "../../chain";
 import { ObservableQuerySecretContractCodeHash } from "./contract-hash";
 import { ObservableQuerySecret20ContractInfo } from "./secret20-contract-info";
 import { DeepReadonly } from "utility-types";
 import { ObservableQuerySecret20BalanceRegistry } from "./secret20-balance";
-import { QueriesWrappedCosmos } from "../cosmos";
 import { OWallet } from "@owallet/types";
+import { QuerySharedContext } from "../../common";
 
-export interface HasSecretQueries {
-  secret: SecretQueries;
+export interface SecretQueries {
+  secret: SecretQueriesImpl;
 }
 
-export class QueriesWrappedSecret
-  extends QueriesWrappedCosmos
-  implements HasSecretQueries
-{
-  public secret: SecretQueries;
-
-  constructor(
-    kvStore: KVStore,
+export const SecretQueries = {
+  use(options: {
+    apiGetter: () => Promise<OWallet | undefined>;
+  }): (
+    queriesSetBase: QueriesSetBase,
+    sharedContext: QuerySharedContext,
     chainId: string,
-    chainGetter: ChainGetter,
-    apiGetter: () => Promise<OWallet | undefined>
-  ) {
-    super(kvStore, chainId, chainGetter);
+    chainGetter: ChainGetter
+  ) => SecretQueries {
+    return (
+      queriesSetBase: QueriesSetBase,
+      sharedContext: QuerySharedContext,
+      chainId: string,
+      chainGetter: ChainGetter
+    ) => {
+      return {
+        secret: new SecretQueriesImpl(
+          queriesSetBase,
+          sharedContext,
+          chainId,
+          chainGetter,
+          options.apiGetter
+        ),
+      };
+    };
+  },
+};
 
-    this.secret = new SecretQueries(
-      this,
-      kvStore,
-      chainId,
-      chainGetter,
-      apiGetter
-    );
-  }
-}
-
-export class SecretQueries {
+export class SecretQueriesImpl {
   public readonly querySecretContractCodeHash: DeepReadonly<ObservableQuerySecretContractCodeHash>;
   public readonly querySecret20ContractInfo: DeepReadonly<ObservableQuerySecret20ContractInfo>;
 
   constructor(
     base: QueriesSetBase,
-    kvStore: KVStore,
+    sharedContext: QuerySharedContext,
     chainId: string,
     chainGetter: ChainGetter,
     apiGetter: () => Promise<OWallet | undefined>
   ) {
     this.querySecretContractCodeHash =
-      new ObservableQuerySecretContractCodeHash(kvStore, chainId, chainGetter);
+      new ObservableQuerySecretContractCodeHash(
+        sharedContext,
+        chainId,
+        chainGetter
+      );
 
     base.queryBalances.addBalanceRegistry(
       new ObservableQuerySecret20BalanceRegistry(
-        kvStore,
+        sharedContext,
         apiGetter,
         this.querySecretContractCodeHash
       )
     );
 
     this.querySecret20ContractInfo = new ObservableQuerySecret20ContractInfo(
-      kvStore,
+      sharedContext,
       chainId,
       chainGetter,
       apiGetter,
