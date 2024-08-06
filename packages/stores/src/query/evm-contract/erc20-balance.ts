eb84d51e59f52b3d550c785b16bfa6f62c3f619d
toan <toan.dq@orai.io>
2024-08-06 16:01:01 +0700

    setup evmContract new core

--- packages/stores/src/query/evm-contract/erc20-balance.ts
+++ packages/stores/src/query/evm-contract/erc20-balance.ts
@@ -1,34 +1,33 @@
 import { computed, makeObservable, override } from "mobx";
 import { DenomHelper, getRpcByChainId, KVStore } from "@owallet/common";
-import { ChainGetter, QueryResponse } from "../../common";
+import { QueryError, QueryResponse, QuerySharedContext } from "../../common";
 import { CoinPretty, Int } from "@owallet/unit";
-import {
-  BalanceRegistry,
-  BalanceRegistryType,
-  ObservableQueryBalanceInner,
-} from "../balances";
+import { BalanceRegistry, IObservableQueryBalanceImpl } from "../balances";
 import { Erc20ContractBalance } from "./types";
 import ERC20_ABI from "human-standard-token-abi";
 import { ObservableChainQuery } from "../chain-query";
 import Web3 from "web3";
+import { ChainGetter } from "src/chain";
+import { AppCurrency } from "@owallet/types";
 
-export class ObservableQueryErc20Balance extends ObservableChainQuery<Erc20ContractBalance> {
+export class ObservableQueryErc20BalancesImplParent extends ObservableChainQuery<Erc20ContractBalance> {
   constructor(
-    kvStore: KVStore,
+    sharedContext: QuerySharedContext,
     chainId: string,
     chainGetter: ChainGetter,
     protected readonly contractAddress: string,
     protected readonly walletAddress: string
   ) {
-    super(kvStore, chainId, chainGetter, contractAddress);
+    super(sharedContext, chainId, chainGetter, contractAddress);
   }
 
   protected canFetch(): boolean {
     return this.contractAddress?.length !== 0 && this.walletAddress !== "";
   }
-  protected async fetchResponse(): Promise<
-    QueryResponse<Erc20ContractBalance>
-  > {
+  protected async fetchResponse(
+    abortController: AbortController
+  ): Promise<{ headers: any; data: Erc20ContractBalance }> {
+    const { headers } = await super.fetchResponse(abortController);
     if (!Web3.utils.isAddress(this.walletAddress)) return;
     try {
       const web3 = new Web3(
@@ -51,9 +50,7 @@ export class ObservableQueryErc20Balance extends ObservableChainQuery<Erc20Contr
 
       return {
         data,
-        status: 1,
-        staled: false,
-        timestamp: Date.now(),
+        headers,
       };
     } catch (error) {
       console.log("🚀 ~ ObservableQueryErc20Balance ~ error:", error);
@@ -61,44 +58,66 @@ export class ObservableQueryErc20Balance extends ObservableChainQuery<Erc20Contr
   }
 }
 
-export class ObservableQueryErc20BalanceInner extends ObservableQueryBalanceInner {
-  protected readonly queryErc20Balance: ObservableQueryErc20Balance;
+export class ObservableQueryErc20BalancesImpl
+  implements IObservableQueryBalanceImpl
+{
+  // protected readonly queryErc20Balance: ObservableQueryErc20Balance;
 
   constructor(
-    kvStore: KVStore,
-    chainId: string,
-    chainGetter: ChainGetter,
-    denomHelper: DenomHelper,
+    protected readonly parent: ObservableQueryErc20BalancesImplParent,
+    protected readonly chainId: string,
+    protected readonly chainGetter: ChainGetter,
+    protected readonly denomHelper: DenomHelper,
     protected readonly walletAddress: string
   ) {
-    super(
-      kvStore,
-      chainId,
-      chainGetter,
-      // No need to set the url at initial.
-      "",
-      denomHelper
-    );
+    // super(
+    //   parent,
+    //   chainId,
+    //   chainGetter,
+    //   // No need to set the url at initial.
+    //   "",
+    //   denomHelper
+    // );
 
     makeObservable(this);
 
-    this.queryErc20Balance = new ObservableQueryErc20Balance(
-      kvStore,
-      chainId,
-      chainGetter,
-      denomHelper.contractAddress,
-      walletAddress
-    );
+    // this.queryErc20Balance = new ObservableQueryErc20Balance(
+    //   kvStore,
+    //   chainId,
+    //   chainGetter,
+    //   denomHelper.contractAddress,
+    //   walletAddress
+    // );
   }
+  @computed
+  get currency(): AppCurrency {
+    const denom = this.denomHelper.denom;
 
+    const chainInfo = this.chainGetter.getChain(this.chainId);
+    return chainInfo.forceFindCurrency(denom);
+  }
   // This method doesn't have the role because the fetching is actually exeucnted in the `ObservableQueryErc20Balance`.
   protected canFetch(): boolean {
     return false;
   }
-
+  get error(): Readonly<QueryError<unknown>> | undefined {
+    return this.parent.error;
+  }
+  get isFetching(): boolean {
+    return this.parent.isFetching;
+  }
+  get isObserved(): boolean {
+    return this.parent.isObserved;
+  }
+  get isStarted(): boolean {
+    return this.parent.isStarted;
+  }
+  get response(): Readonly<QueryResponse<Erc20ContractBalance>> | undefined {
+    return this.parent.response;
+  }
   @override
   *fetch() {
-    yield this.queryErc20Balance.fetch();
+    yield this.parent.fetch();
   }
 
   @computed
@@ -114,47 +133,67 @@ export class ObservableQueryErc20BalanceInner extends ObservableQueryBalanceInne
       throw new Error(`Unknown currency: ${denom}`);
     }
 
-    if (
-      !this.queryErc20Balance.response ||
-      !this.queryErc20Balance.response.data.balance
-    ) {
+    if (!this.parent.response || !this.parent.response.data.balance) {
       return new CoinPretty(currency, new Int(0)).ready(false);
     }
 
-    return new CoinPretty(
-      currency,
-      new Int(this.queryErc20Balance.response.data.balance)
-    );
+    return new CoinPretty(currency, new Int(this.parent.response.data.balance));
+  }
+  async waitFreshResponse(): Promise<
+    Readonly<QueryResponse<unknown>> | undefined
+  > {
+    return await this.parent.waitFreshResponse();
+  }
+
+  async waitResponse(): Promise<Readonly<QueryResponse<unknown>> | undefined> {
+    return await this.parent.waitResponse();
   }
 }
 
 export class ObservableQueryErc20BalanceRegistry implements BalanceRegistry {
-  readonly type: BalanceRegistryType = "erc20";
+  // readonly type: BalanceRegistryType = "erc20";
+  protected parentMap: Map<string, ObservableQueryErc20BalancesImplParent> =
+    new Map();
+  constructor(protected readonly sharedContext: QuerySharedContext) {}
 
-  constructor(protected readonly kvStore: KVStore) {}
-
-  getBalanceInner(
+  getBalanceImpl(
     chainId: string,
     chainGetter: ChainGetter,
     walletAddress: string,
     minimalDenom: string
-  ): ObservableQueryBalanceInner | undefined {
+  ): IObservableQueryBalanceImpl | undefined {
     const denomHelper = new DenomHelper(minimalDenom);
-    if (denomHelper.type !== "erc20") {
-      console.log(denomHelper, "denomHelper native");
-    }
-    if (denomHelper.type === "erc20") {
-      console.log(
-        "🚀 ~ ObservableQueryErc20BalanceRegistry ~ denomHelper:",
-        denomHelper
+    const key = `${chainId}/${walletAddress}`;
+    if (denomHelper.type !== "erc20") return;
+
+    // return new ObservableQueryErc20BalancesImpl(
+    //   this.parentMap.get(key)!,
+    //   chainId,
+    //   chainGetter,
+    //   denomHelper,
+    //   walletAddress
+    // );
+    // const key = `${chainId}/${bech32Address}`;
+
+    if (!this.parentMap.has(key)) {
+      this.parentMap.set(
+        key,
+        new ObservableQueryErc20BalancesImplParent(
+          this.sharedContext,
+          chainId,
+          chainGetter,
+          denomHelper.contractAddress,
+          walletAddress
+        )
       );
-      return new ObservableQueryErc20BalanceInner(
-        this.kvStore,
-        chainId,
-        chainGetter,
-        denomHelper,
-        walletAddress
-      );
     }
+
+    return new ObservableQueryErc20BalancesImpl(
+      this.parentMap.get(key)!,
+      chainId,
+      chainGetter,
+      denomHelper,
+      walletAddress
+    );
   }
 }
--- packages/stores/src/query/evm-contract/gas/index.ts
+++ packages/stores/src/query/evm-contract/gas/index.ts
@@ -3,11 +3,12 @@ import {
   ObservableChainQueryMap,
 } from "../../chain-query";
 import { getRpcByChainId, KVStore } from "@owallet/common";
-import { ChainGetter, QueryResponse } from "../../../common";
+import { QueryResponse, QuerySharedContext } from "../../../common";
 import { computed, makeObservable } from "mobx";
 import Web3 from "web3";
 import { utils } from "ethers";
 import ERC20_ABI from "human-standard-token-abi";
+import { ChainGetter } from "src/chain";
 type GasEvmRequest = {
   to: string;
   from: string;
@@ -16,12 +17,12 @@ type GasEvmRequest = {
 };
 export class ObservableQueryGasEvmContractInner extends ObservableChainQuery<number> {
   constructor(
-    kvStore: KVStore,
+    sharedContext: QuerySharedContext,
     chainId: string,
     chainGetter: ChainGetter,
     protected readonly paramGas: GasEvmRequest
   ) {
-    super(kvStore, chainId, chainGetter, ``);
+    super(sharedContext, chainId, chainGetter, ``);
     makeObservable(this);
   }
 
@@ -37,8 +38,11 @@ export class ObservableQueryGasEvmContractInner extends ObservableChainQuery<num
     }
     return this.response.data;
   }
-  protected async fetchResponse(): Promise<QueryResponse<number>> {
+  protected async fetchResponse(
+    abortController: AbortController
+  ): Promise<{ headers: any; data: number }> {
     try {
+      const { headers } = await super.fetchResponse(abortController);
       const web3 = new Web3(
         getRpcByChainId(this.chainGetter.getChain(this.chainId), this.chainId)
       );
@@ -58,12 +62,7 @@ export class ObservableQueryGasEvmContractInner extends ObservableChainQuery<num
           from: from,
         });
 
-      return {
-        status: 1,
-        staled: false,
-        data: estimateGas,
-        timestamp: Date.now(),
-      };
+      return { headers, data: estimateGas };
     } catch (error) {
       console.log(
         "🚀 ~ ObservableQueryGasEvmContractInner ~ fetchResponse ~ error:",
@@ -71,22 +70,17 @@ export class ObservableQueryGasEvmContractInner extends ObservableChainQuery<num
       );
     }
   }
-  protected getCacheKey(): string {
-    return `${this.instance.name}-${
-      this.instance.defaults.baseURL
-    }-gas-evm-contract-native-${this.chainId}-${JSON.stringify(this.paramGas)}`;
-  }
 }
 
 export class ObservableQueryGasEvmContract extends ObservableChainQueryMap<number> {
   constructor(
-    protected readonly kvStore: KVStore,
+    protected readonly sharedContext: QuerySharedContext,
     protected readonly chainId: string,
     protected readonly chainGetter: ChainGetter
   ) {
-    super(kvStore, chainId, chainGetter, (data) => {
+    super(sharedContext, chainId, chainGetter, (data) => {
       return new ObservableQueryGasEvmContractInner(
-        this.kvStore,
+        this.sharedContext,
         this.chainId,
         this.chainGetter,
         JSON.parse(data)
